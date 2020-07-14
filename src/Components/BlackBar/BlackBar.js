3b24a33f72ea668d1ba5a9d411996b4610715146
Greg Johnston <gcjohnston91@gmail.com>
2020-07-14 16:08:32 -0400

    Working on header

---
+++ src/Components/BlackBar/BlackBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BlackBar.scss'
+import {Grid} from "react-feather";
+
+const BlackBar = () => {
+  return (
+    <div className='black-bar'>
+      <Grid className='feather-grid'/>
+    </div>
+  );
+};
+
+export default BlackBar;
